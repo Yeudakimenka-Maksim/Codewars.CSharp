c8d56da16a54ddf35c0f69b4a4a1a3f3897daab2
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2015-08-22 02:35:32 +0300

    First version of 'Merged String Checker' kata. http://www.codewars.com/kata/54c9fcad28ec4c6e680011aa

---
+++ 54c9fcad28ec4c6e680011aa/MergedStringChecker.Tests/StringMergerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace MergedStringChecker.Tests
+{
+  [TestFixture]
+  public class StringMergerTests
+  {
+    [Test]
+    public void HappyPath1()
+    {
+      Assert.IsTrue(StringMerger.isMerge("codewars", "code", "wars"), "codewars can be created from code and wars");
+    }
+
+    [Test]
+    public void HappyPath2()
+    {
+      Assert.IsTrue(StringMerger.isMerge("codewars", "cdwr", "oeas"), "codewars can be created from cdwr and oeas");
+    }
+
+    [Test]
+    public void HappyPath4()
+    {
+      Assert.IsTrue(
+        StringMerger.isMerge("Can we merge it? Yes, we can!", "n ee tYw n!", "Cawe mrgi? es, eca"),
+        "Can we merge it? Yes, we can!' is a merge of 'n ee tYw n!' and 'Cawe mrgi? es, eca");
+    }
+
+    [Test]
+    public void SadPath1()
+    {
+      Assert.IsFalse(StringMerger.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
+    }
+  }
+}
---
+++ 54c9fcad28ec4c6e680011aa/MergedStringChecker/StringMerger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace MergedStringChecker
+{
+  public class StringMerger
+  {
+    public static bool isMerge(string s, string part1, string part2)
+    {
+      var builder1 = new StringBuilder(part1);
+      var builder2 = new StringBuilder(part2);
+
+      for (var i = 0; i < s.Length; i++)
+      {
+        var result = ProcessSymbol(s[i], builder1, builder2);
+        var ambiguity = result.Item1;
+        var mismatch = result.Item2 == false;
+
+        if (ambiguity)
+        {
+          return
+            isMerge(s.Substring(i + 1), builder1.ToString(), builder2.Remove(0, 1).ToString()) ||
+            isMerge(s.Substring(i + 1), builder1.Remove(0, 1).ToString(), builder2.ToString());
+        }
+        else if (mismatch)
+        {
+          return false;
+        }
+      }
+
+      if (builder1.Length != 0 || builder2.Length != 0)
+      {
+        return false;
+      }
+
+      return true;
+    }
+
+    private static Tuple<bool, bool> ProcessSymbol(char symbol, StringBuilder builder1, StringBuilder builder2)
+    {
+      if (builder1.Length != 0 && builder2.Length != 0 && builder1[0] == symbol && builder2[0] == symbol)
+      {
+        return Tuple.Create(true, true);
+      }
+
+      if (builder1.Length != 0 && builder1[0] == symbol)
+      {
+        builder1.Remove(0, 1);
+        return Tuple.Create(false, true);
+      }
+      else if (builder2.Length != 0 && builder2[0] == symbol)
+      {
+        builder2.Remove(0, 1);
+        return Tuple.Create(false, true);
+      }
+
+      return Tuple.Create(false, false);
+    }
+  }
+}
