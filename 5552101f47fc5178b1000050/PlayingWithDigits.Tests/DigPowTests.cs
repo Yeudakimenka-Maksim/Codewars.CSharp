9956ba51dd877ca343aa062fdc922a3fa09b6815
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2015-08-22 00:21:43 +0300

    Solved 'Playing with digits' kata. http://www.codewars.com/kata/5552101f47fc5178b1000050

---
+++ 5552101f47fc5178b1000050/PlayingWithDigits.Tests/DigPowTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace PlayingWithDigits.Tests
+{
+  [TestFixture]
+  public class DigPowTests
+  {
+    [Test]
+    public void Test1()
+    {
+      Assert.AreEqual(1, DigPow.digPow(89, 1));
+    }
+
+    [Test]
+    public void Test2()
+    {
+      Assert.AreEqual(-1, DigPow.digPow(92, 1));
+    }
+
+    [Test]
+    public void Test3()
+    {
+      Assert.AreEqual(51, DigPow.digPow(46288, 3));
+    }
+  }
+}
---
+++ 5552101f47fc5178b1000050/PlayingWithDigits/DigPow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlayingWithDigits
+{
+  public class DigPow
+  {
+    public static long digPow(int n, int p)
+    {
+      var sum = 0;
+      foreach (var c in n.ToString())
+      {
+        sum += (int)Math.Pow((int)char.GetNumericValue(c), p++);
+      }
+
+      if (sum % n == 0)
+      {
+        return sum / n;
+      }
+
+      return -1;
+    }
+  }
+}
