ada99d903c1baab2b66c76a63ce45e5d4ca51ea9
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2015-08-22 15:57:36 +0300

    Solved 'Credit Card Mask' kata. http://www.codewars.com/kata/5412509bd436bd33920011bc

---
+++ 5412509bd436bd33920011bc/CreditCardMask.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CreditCardMask.Tests
+{
+  [TestFixture]
+  public class Tests
+  {
+    [Test]
+    public void ExamplesTests()
+    {
+      Assert.AreEqual(Kata.Maskify("4556364607935616"), "############5616");
+      Assert.AreEqual(Kata.Maskify("1"), "1");
+      Assert.AreEqual(Kata.Maskify("11111"), "#1111");
+    }
+  }
+}
---
+++ 5412509bd436bd33920011bc/CreditCardMask/Kata.cs
@@ -0,0 +1,11 @@
+﻿namespace CreditCardMask
+{
+  public static class Kata
+  {
+    // return masked string
+    public static string Maskify(string cc)
+    {
+      return cc.Length <= 4 ? cc : cc.Substring(cc.Length - 4).PadLeft(cc.Length, '#');
+    }
+  }
+}
